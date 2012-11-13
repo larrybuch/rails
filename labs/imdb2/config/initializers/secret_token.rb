1cf19933d0f34c4244ced2dc79e6f7361016203e
Larry Buchanan <larrybuch@gmail.com>
2012-11-13 11:08:38 -0500

    adding imdb2 rails app

---
+++ labs/imdb2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imdb2::Application.config.secret_token = '103c9b77de3811d0db0667dbf2441b76b7b3728cef1b222b07f067ba24d48647ea31941d222474bb19e57aff7a8a3d21391ff9c1e0ea9fb11a60fbd1f9deaea5'
