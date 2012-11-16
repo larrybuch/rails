dceb89a17dec53ecabac7719c0382bf664bcbb36
Larry Buchanan <larrybuch@gmail.com>
2012-11-16 10:10:27 -0500

    first rails quiz, adding boxes

---
+++ quizzes/q-2012-11-16-1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Q201211161::Application.config.secret_token = 'fe80046424b20df56f44f1d5d1141164e742c5a41318726af6444ee7478e93d93b7a8801c131b0484fe82351f1ded459b8e42720fc27b25d0d4d6d97a771aaa2'
