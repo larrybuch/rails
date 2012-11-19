88ee3a935473c4385b06a82335dbf291e5ee9197
Larry Buchanan <larrybuch@gmail.com>
2012-11-19 13:28:27 -0500

    abit more

---
+++ labs/slideshow/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slideshow::Application.config.secret_token = 'f474e2eeba3348b2c56b782cc35b5a920dc0c04fa5d5f8215bd582e3c4ba6f247d36137501841772fe337d8d1c8640cf1272ebf6ba0cda9344d9db3a431d88ec'
