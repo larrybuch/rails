8df6e4a3cd7cdf8de5feb9e9eda3433f5e76cb7f
Larry Buchanan <larrybuch@gmail.com>
2012-11-20 14:53:42 -0500

    big ol commit

---
+++ labs/cookiedemo/app/controllers/session_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    ninja = Ninja.find_by_name(params[:name])
+    if ninja && ninja.authenticate(params[:password]) #logged in (if ninja and password match, log in)
+      session[:user_id] = ninja.id
+      redirect_to ninjas_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to ninjas_path
+  end
+
+end
---
+++ labs/cookiedemo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookiedemo::Application.config.secret_token = 'baf829056b90b8c3ffa63697b8833778bbce0d5b8f214da572e236911db313a30628850752fbbc16159190ed01e184696b5372b51f7240d522ed868b33fc2f4c'
