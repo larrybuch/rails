d6aed565e44b90941e71873cb40f1c65ceaeb3fd
Larry Buchanan <larrybuch@gmail.com>
2012-11-26 10:05:04 -0500

    clearing directory

---
+++ labs/cookiedemo/db/migrate/20121120201002_add_photos_table.rb
@@ -0,0 +1,7 @@
+class AddPhotosTable < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
---
+++ labs/cookiedemo/db/migrate/20121120201056_add_messages_table.rb
@@ -0,0 +1,7 @@
+class AddMessagesTable < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
