e5fbae9044e8bfeed760312ec711cb9881d8d3c1
Larry Buchanan <larrybuch@gmail.com>
2012-11-29 10:21:42 -0500

    hydrogen quiz with boxes

---
+++ quizzes/hydrogen/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydrogen::Application.config.secret_token = '27b7455bb257183b7ec63e993497189bcb24f112d033a30e027dc64f8202ced00d977f8c696559a72be6fee20235fbd26f11708fde4b7d8eafa63544a8336244'
