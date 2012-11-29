c1e77f02be27c9e246b5dd552bfbba0854db8207
Larry Buchanan <larrybuch@gmail.com>
2012-11-29 11:00:11 -0500

    helium

---
+++ quizzes/helium/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helium::Application.config.secret_token = '20f79087bf90a13bdfc65a3cf0ecffd89cc5eb16f1a29e5780149f7d1c781ab9f6d10152674fcf71368a53956db8158faec337f7df0ceffd7cc489eb15167361'
