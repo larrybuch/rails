9e19353a55d7d17c9272da02108fe15e867d78d7
Larry Buchanan <larrybuch@gmail.com>
2012-11-30 15:59:56 -0500

    pushing the lab

---
+++ labs/monstrosity/pirate-service/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PirateService::Application.config.secret_token = '1ae5e0881fa3d6e7cd02390d5f7f677d4a7359c5105e63d45c01f43e9886220d55b36046546e23355f9e05fcda4f9ebc51d0d5978972e199f9ffcc23dd87aec4'
