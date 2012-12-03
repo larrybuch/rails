2dc90118f22c238c08ee0547fdb9e4f1fd7344f2
Larry Buchanan <larrybuch@gmail.com>
2012-12-03 11:05:02 -0500

    built stack from scratch

---
+++ labs/stack-demo/app/models/stack.rb
@@ -0,0 +1,17 @@
+class Stack
+  def initialize
+    @data = []
+  end
+
+  def push(value)
+    @data[@data.count] = value
+  end
+
+  def pop
+    @data.delete(@data[-1])
+  end
+
+  def to_s
+    "there are #{@data.length} items in this stack: #{@data}"
+  end
+end
---
+++ labs/stack-demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackDemo::Application.config.secret_token = 'b6046cfd63358533953afa77bf1881839263b3a5675511c4f9079be5093cba5aad3dbc4465ade09d313e6601e4d3b9940f2c89b8c44b167613c91b00706df408'
