bb8f2f9e96464371516ff9483259458b07bbb98c
Larry Buchanan <larrybuch@gmail.com>
2012-12-20 13:45:40 -0500

    clearing branch

---
+++ labs/haml_vs_vampires/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamlVsVampires::Application.config.secret_token = '68a3580a4d8796adda6555d23c14455f4af8a8d1968d0be2779c2fd0a14a12d9b698583414871326c451973f4b53fcaf096c9954479742e1a541b9c48e97e0aa'
